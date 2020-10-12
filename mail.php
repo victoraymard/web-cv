668923e61b18a2980382a4be73914cdc09f7ae5b
bosetti <victor.aymard@edu.ece.fr>
2020-10-12 12:20:48 +0200

    mail

--- mail.php
+++ mail.php
@@ -9,9 +9,13 @@
 
 <body>
 <?php
-$retour = mail('victor.aymard@live.fr', 'Envoi depuis la page Contact', $_POST['message'], 'From : webmaster@monsite.fr');
+/*$retour = mail('victor.aymard@live.fr', 'Envoi depuis la page Contact', $_POST['message'], 'From : webmaster@monsite.fr');*/
+$retour = mail('victor.aymard@edu.ece.fr', 'Envoi depuis la page Contact', $_POST['message'], 'From : webmaster@monsite.fr');
 if ($retour) {
     echo '<p>Votre message a bien été envoyé.</p>';
+}
+else {
+    echo '<p>Il y a eu un probleme au niveau de l envoie</p>';
 }
 ?>
 
