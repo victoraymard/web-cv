bae8ff3f0a395eac3a729ccebffae050695781db
bosetti <victor.aymard@edu.ece.fr>
2020-10-12 11:53:17 +0200

    test mail php

--- mail.php
+++ mail.php
@@ -1,3 +1,5 @@
+
+
 <!doctype html>
 <html>
 <head>
@@ -6,26 +8,11 @@
 </head>
 
 <body>
-    <?php
-    /*$name = $_POST['submit'];*/
-    $subject = $_POST ['subject'];
-    $mailfrom = $_POST['email'];
-    $message = $_POST['message'];
-
-    $mailTo = "victor.aymard@edu.ece.fr";
-    $txt = $_POST['name']. " avec comme adresse: ".$mailfrom. " et comme objet : " .$subject. " vous a envoyé le message suivant : ";
-    $txt .= $message;
-    $retour = mail($mailTo, "Envoi depuis la page contact" + $txt, $txt, 'From : webmaster@monsite.fr');
-    header('Location: victoraymard.yo.fr');
-     if ($retour) {
-        echo '<p>Votre message a bien été envoyé.</p>';
-    }
-
-
-   /* $retour = mail('victor.aymard@edu.ece.fr', 'Envoi depuis la page Contact', $_POST['message'], 'From : webmaster@monsite.fr');
-    if ($retour) {
-        echo '<p>Votre message a bien été envoyé.</p>';
-    }*/
-    ?>
+<?php
+$retour = mail('victor.aymard@live.fr', 'Envoi depuis la page Contact', $_POST['message'], 'From : webmaster@monsite.fr');
+if ($retour) {
+    echo '<p>Votre message a bien été envoyé.</p>';
+}
+?>
 </body>
 </html>
