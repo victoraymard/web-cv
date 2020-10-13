6e5ee0e50430ef066b8a6bc24578b3f9cff9295a
bosetti <victor.aymard@edu.ece.fr>
2020-10-13 14:57:24 +0200

    test

--- _mail.php
+++ _mail.php
@@ -1,8 +1,13 @@
 <?php
-require 'lib/mailjetPHP/';
 
-use lib\mailjetPHP\src\Mailjet\Resources;
-$mj = new \Mailjet\Client('2423def9caeb967083ac0089d90ffcaf','47e244ed9285db75ae5d4398423f9fe5',true,['version' => 'v3.1']);
+require 'lib/mailjetPHP/src/Mailjet/Client.php';
+require 'lib/mailjetPHP/src/Mailjet/Resources.php';
+
+use Mailjet\Client;
+use Mailjet\Resources;
+
+
+$mj = new Client('2423def9caeb967083ac0089d90ffcaf','47e244ed9285db75ae5d4398423f9fe5',true,['version' => 'v3.1']);
 
 $body = [
     'Messages' => [
@@ -18,8 +23,8 @@ $body = [
                 ]
             ],
             'Subject' => "Greetings from Mailjet.",
-            'TextPart' => "My first Mailjet email " + $_POST['message'],
-            'HTMLPart' => "<h3>Dear passenger 1, welcome to <a href='https://www.mailjet.com/'>Mailjet</a>!</h3><br />May the delivery force be with you!"+ $_POST['message'],
+            'TextPart' => "My first Mailjet email " ,
+            'HTMLPart' => "<h3>Dear passenger 1, welcome to <a href='https://www.mailjet.com/'>Mailjet</a>!</h3><br />May the delivery force be with you!",
             'CustomID' => "AppGettingStartedTest"
         ]
     ]
